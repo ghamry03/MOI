7824af75650e363c8fbb493272e5264fb6d904a4
Omar <omarelghamry@icloud.com>
2024-05-14 22:14:20 +0400

    prep for routing

--- frontend/src/components/LandingPage.vue
+++ frontend/src/components/LandingPage.vue
@@ -1,14 +1,67 @@
+<template>
+    <div class="flex flex-col gap-9 md:gap-6">
+        <TopBar />
+        <TopSection />
+        <Services />
+    </div>
+</template>
 
 <script>
 import TopBar from './components/TopBar'
-import ProfileCard from './components/ProfileCard.vue'
-import FinesCard from './components/FinesCard.vue'
+import TopSection from './components/TopSection.vue'
+import Services from './components/ServicesSection.vue'
 
 export default {
     name: 'App',
     components: {
-        ProfileCard,
-        FinesCard
+        TopBar,
+        TopSection,
+        Services
     }
 }
 </script>
+
+<style>
+@import '@/assets/css/tailwind.css';
+@import '@/assets/css/global.css';
+
+:root {
+    --background-color: #FFFFFF;
+    --dark-white: #F3F2F2;
+    --gold-color: #DCBA6A;
+    --gold-highlight: #CF9F30;
+    --text-color: #000000;
+
+}
+
+body {
+    background-color: var(--background-color);
+    color: var(--text-color);
+    padding: 24px;
+}
+
+.logoFont {
+    font-family: "SF Pro Text";
+    font-style: normal;
+    font-weight: 900;
+}
+
+.topBarFont {
+    font-family: "SF Pro Text";
+    font-style: normal;
+    font-weight: 600;
+}
+
+.navBar {}
+
+.navBarSelected {}
+
+.subTitle {
+    font-family: "SF Pro Text";
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+}
+
+.text {}
+</style>
